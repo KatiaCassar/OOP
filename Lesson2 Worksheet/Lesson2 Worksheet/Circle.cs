56b67655f12c925880c89b57a568ef570f1dc48f
Katia Cassar <katiacassar@gmail.com>
2020-10-23 11:59:17 +0200

    Lesson 2: Intro to Objects and Constructors

---
+++ Lesson2 Worksheet/Lesson2 Worksheet/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Worksheet
+{
+    //access modifier - are the level of access to the class and its members
+    //eg. Public, Private, Protected
+    public class Circle
+    {
+        //constructor - bears the same name as the class, which is used to create an instance of the class (the object)
+        //              secondary objective of construct : it can be used as a normal method
+        public Circle(double radius, string colour) // the constructors is like the key to start the class / the key to initialize the object
+        {
+            Radius = radius; //setting to default values
+            Colour = colour;
+        }
+
+        public string Colour { get; set; }
+        public double Radius { get; set; }
+
+        //public double Radius { get; set; } //property
+
+        /// <summery>
+        /// display the radius value of the instance when called
+        /// </summery>
+        /// <returns></returns>
+        
+        public string GetRadius()
+        {
+            return "Radius: " + Radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
---
+++ Lesson2 Worksheet/Lesson2 Worksheet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Worksheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object (instance) ->> object is when you create a space in memory and start assigning values to the template you created earlier
+            Circle c = new Circle(5, "black"); //eg. of ac object --> c
+            
+            c.Radius = 5;
+
+            Console.WriteLine("Input a radius: ");
+            c.Radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Area is {c.GetArea()}");
+            Console.WriteLine($"Perimeter is {c.GetPerimeter()}");
+
+            Console.WriteLine($"Press a key to terminate");
+            Console.ReadKey();
+        }
+    }
+}

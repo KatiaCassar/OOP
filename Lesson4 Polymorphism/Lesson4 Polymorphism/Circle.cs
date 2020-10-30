8b6dce3fd322c7527af9f51a7561741ee32d24da
Katia Cassar <katiacassar@gmail.com>
2020-10-30 11:56:32 +0100

    Lesson 4 : Dynamic & Static Polymorphism

---
+++ Lesson4 Polymorphism/Lesson4 Polymorphism/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_Polymorphism
+{
+    class Circle : Point
+    {
+        public Circle (int x, int y, double radius) :base(x, y)
+        {
+            //X = x;    //these 2 are ommited because you are taking them from the base constructor using the :base(x, y)
+            //Y = y; 
+            Radius = radius;
+        }
+        public double Radius { get; set; }
+
+        public override void Draw()
+        {
+            // base.Draw();     //this is ommmited to compleately change the method and not the add to it
+            Console.WriteLine("Drawig a Circle");
+        }
+
+        public virtual double FindArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
---
+++ Lesson4 Polymorphism/Lesson4 Polymorphism/Cylinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_Polymorphism
+{
+    class Cylinder : Circle
+    {
+        public double Length { get; set; }
+
+        public Cylinder(int x, int y, double radius, double length) : base(x, y, radius)
+        {
+            Length = length;
+        }
+
+        public override double FindArea()
+        {
+            return (2 * Math.PI * Radius *Length) + (2 * Math.PI * Math.Pow(Radius, 2));
+        }
+    }
+}

f7fa74206aedb77bf5bb21e1c6032abe5706cb38
Katia Cassar <katiacassar@gmail.com>
2020-11-05 15:32:26 +0100

    Lesson 5 Polymorphism

---
+++ Lesson5 Polymorphism ShapesLibrary/Lesson5 Polymorphism ShapesLibrary/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_Polymorphism_ShapesLibrary
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(int x, int y, double radius) : base(x, y)
+        {
+            Radius = radius;
+        }
+    }
+}
